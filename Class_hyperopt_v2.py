fc97aa6e5aa20a7ea55c358909c2f07c64102fde
LIN MUJIE <110078544+Linmj-Judy@users.noreply.github.com>
2024-04-19 21:54:13 +0800

    Update Class_hyperopt_v2.py by fixing bugs

--- Class_hyperopt_v2.py
+++ Class_hyperopt_v2.py
@@ -51,7 +51,7 @@ def init_pretained_model(arch):
     dummy_input = tf.zeros([1, sequence_length], dtype=tf.int32)  # 假设输入是整数类型的token索引
     
     # 创建虚拟的adjoin_matrix和mask
-    dummy_adjoin_matrix = tf.zeros([1, sequence_length, sequence_length], dtype=tf.float32)  
+    dummy_adjoin_matrix = tf.zeros([1, sequence_length, sequence_length], dtype=tf.float32)  # 根据实际需要可能需要调整类型
     dummy_mask = tf.ones([1, sequence_length], dtype=tf.bool)  # 假设所有位置都是有效的
     dummy_mask = tf.where(dummy_mask, -1e9, 0.0)
     # 使用虚拟输入调用模型
@@ -74,7 +74,7 @@ def transfer_pretrained_encoder_weights(source_model, pretraining: bool):
     dense_dropout = 0.1
     sequence_length = 128
     label = ['standard_value']
-
+    # 假设源模型和目标模型的编码器权重是直接兼容的
     # 初始化目标模型
     target_model = PredictModel(num_layers=num_layers, d_model=d_model, dff=dff,
                                 num_heads=num_heads, vocab_size=vocab_size,
@@ -107,9 +107,9 @@ def count_parameters(model):
         total_params += params
     return total_params
 
-def load_dataset(task, batch_size, label, seed):
+def load_dataset(data_file, batch_size, label, seed):
     
-    graph_dataset = Graph_Classification_Dataset(task + '.csv', smiles_field='canonical_smiles',
+    graph_dataset = Graph_Classification_Dataset(data_file, smiles_field='canonical_smiles',
                                                  label_field=label, seed=seed,
                                                  batch_size=batch_size, a=len(label),
                                                  addH=True)
@@ -226,11 +226,14 @@ def evaluate_test(test_dataset, FTmodel, label):
     test_y_t = []
     test_y_p = []
     for i in range(len(label)):
+        # 合并所有批次的真实标签和预测结果
         try:
             y_label = np.concatenate([np.array(batch[i]) for batch in y_true if np.array(batch[i]).ndim != 0])
             y_pred = np.concatenate([np.array(batch[i]) for batch in y_preds if np.array(batch[i]).ndim != 0])
         except:
+            # 合并第i个标签的所有批次的真实值
             y_label = np.concatenate(y_true[i])
+            # 合并第i个标签的所有批次的预测值
             y_pred = np.concatenate(y_preds[i])
         
         validId = np.where((y_label == 0) | (y_label == 1))[0]
@@ -253,22 +256,30 @@ def evaluate_test(test_dataset, FTmodel, label):
 
     return test_auc, auc_list, test_y_t, test_y_p #对于每一个标签都有一个test_y_t元组和test_y_p元组，所以最终返回的test_y_t, test_y_p是元组的元组 [[],[],[]]
 
-def main(task, label, seed, args, FTmodel, pretraining=True):
+def main(task, data_file, label, seed, args, FTmodel, pretraining=True):
     pretraining_str = 'pretraining' if pretraining else ''
     
+    
+
     num_heads, dense_dropout, learning_rate, batch_size = load_hyperparam(args)
     
     #环境初始化
     np.random.seed(seed=seed)
     tf.random.set_seed(seed=seed)
 
-    train_dataset, test_dataset, val_dataset = load_dataset(task, batch_size, label, seed)
+    train_dataset, test_dataset, val_dataset = load_dataset(data_file, batch_size, label, seed)
+
+    # 第一次反向传播 #但是有必要在这里计算吗？
+    # x, adjoin_matrix, y = next(iter(train_dataset.take(1)))
+    # mask = tf.cast(tf.math.equal(x, 0), tf.float32)[:, tf.newaxis, tf.newaxis, :]
+
     
     total_params = count_parameters(FTmodel)
     print('*'*100)
     print("Total Parameters:", total_params)
     print('*'*100)
     
+    # def init_train(learning_rate)----
     # 定义模型、优化器和损失函数
     optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
     loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
@@ -280,8 +291,10 @@ def main(task, label, seed, args, FTmodel, pretraining=True):
 
     # 初始化二分类AUC计算器：假设有一个二分类问题或多个二分类标签
     # 注意：
-    # 模型输出是 logits（即未经过 sigmoid 或 softmax 激活的原始输出），因此在初始化 tf.keras.metrics.AUC 时设置 from_logits=True。
+    # 如果模型输出是 logits（即未经过 sigmoid 或 softmax 激活的原始输出），请确保在初始化 tf.keras.metrics.AUC 时设置 from_logits=True。
+    # 如果模型输出已经是概率（经过了激活函数处理），应将其设置为 False。
     auc_metrics = [tf.keras.metrics.AUC(from_logits=True) for _ in range(len(label))]
+    # -----
 
     best_val_auc = -10
     train_loss = []
@@ -358,7 +371,7 @@ def hy_main(args):
     label = ['standard_value']
     for seed in [1231]: #可以做k折交叉验证
         print(seed)  #需要的auc, test_auc, a_list
-        val_auc, test_auc, a_list, test_y_t, test_y_p = main(task, label, seed, args, FTmodel, pretraining=True)
+        val_auc, test_auc, a_list, test_y_t, test_y_p = main(task, data_file, label, seed, args, FTmodel, pretraining=True)
         # 更新包装器
         best_model_wrapper.update(val_auc, test_auc, test_y_t, test_y_p, args)
 
@@ -394,6 +407,22 @@ def score(y_test, y_pred):
     BA = (se + sp) / 2
     return tp, tn, fn, fp, se, sp, mcc, q, auc_roc_score, F1, BA, prauc
 
+
+# def score_all(args):  #实际上，这里只需要接收的参数是args里面的训练参数以及得到的y_true_final, y_pred_final（这个可以另外传进来）
+#     idx = 0
+#     for seed in [1]:  #多个seed有多个结果的没必要重复计算，而是应该在
+#         print(seed)
+#         auc, test_auc, a_list, y_true_final, y_pred_final = main(seed, args)
+#         if idx == 0:
+#             writer.writerow(['tasks', 'tp', 'tn', 'fn',
+#                              'fp', 'se', 'sp', 'mcc', 'q', 'auc_roc_score', 'F1', 'BA', 'prauc',
+#                              'num_heads', "batch_size", "learning_rate", "dense_dropout"])
+#             idx = 1
+#         tp, tn, fn, fp, se, sp, mcc, q, auc_roc_score, F1, BA, prauc = score(y_true_final, y_pred_final)
+#         writer.writerow([task, tp, tn, fn, fp, se, sp, mcc, q, auc_roc_score, F1, BA, prauc,
+#                          args["num_heads"], args["batch_size"], args["learning_rate"], args["dense_dropout"]])
+#     return None
+
 # 检查文件是否存在且具有要求的标题行，如果没有该文件/该标题行，则创建一个具有要求的标题行的文件
 def check_header(file_path, expected_header):
     # 检查文件是否存在
@@ -427,7 +456,9 @@ if __name__ == "__main__":
 
     task = sys.argv[1]
     print(task)
-
+    print(f"Received {len(sys.argv)} arguments: {sys.argv}")
+    data_dir_path = f'{YOUR_DATA_PATH}' #-------Please add the dir path of your datasets here!-------
+    data_file = os.path.join(data_dir_path,f"{task}.csv")
     arch = {'name': 'Medium', 'path': 'medium3_weights'}
 
     source_model = init_pretained_model(arch) #在这里一次性加载预训练权重
@@ -444,7 +475,7 @@ if __name__ == "__main__":
     print("Best Test AUC:", best_model_wrapper.best_auc)
     print("Best Hyperparameters:", best_model_wrapper.best_params)
     
-    a = [8, 16, 32, 48, 64]
+    a = [64,128,256]
     b = [4, 8]
     # 使用字典推导和条件表达式简化赋值过程
     best_dict = {
